c747e1b4854c723888c620ee4f966c8fd2f907d4
Rollxtrem <rrescude@bancolombia.com>
2025-03-28 17:16:47 -0500

    elimitar

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // IMPORTANTE para que Vite genere rutas correctas
-})
